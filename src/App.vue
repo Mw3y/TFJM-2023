59ba7a7ee45c5df587e50bd73a644bc7bf88849e
Mw3y <maxence1604.minecraft@gmail.com>
2023-03-15 15:49:49 +0100

    🎉 First commit

---
+++ src/App.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+	import { useWindowSize, watchThrottled } from "@vueuse/core";
+	import { onMounted, ref } from "vue";
+
+	import {
+		AxesHelper,
+		Box3,
+		Color,
+		EdgesGeometry,
+		Group,
+		LineBasicMaterial,
+		LineSegments,
+		Mesh,
+		MeshBasicMaterial,
+		PerspectiveCamera,
+		PlaneGeometry,
+		Scene,
+		Vector3,
+		WebGLRenderer,
+	} from "three";
+
+	let renderer: WebGLRenderer;
+	const { width, height } = useWindowSize();
+	const [scene, canvas] = [new Scene(), ref(null)];
+
+	// Setup the scene camera
+	const camera = new PerspectiveCamera(50, width.value / height.value);
+	camera.position.set(0, 0, 8);
+	scene.add(camera);
+
+	/**
+	 * Watch for a window resize.
+	 */
+	watchThrottled(
+		[width, height],
+		() => {
+			camera.aspect = width.value / height.value;
+			camera.updateProjectionMatrix();
+
+			renderer.setSize(width.value, height.value);
+			renderer.render(scene, camera);
+		},
+		{ throttle: 250 }
+	);
+
+	const group = new Group();
+	for (let i = 0; i < 5; i++) {
+		// Create the mesh of a new note
+		const square = new Mesh(
+			new PlaneGeometry(1, 1, 1, 1),
+			new MeshBasicMaterial({
+				color: new Color().setHex(Math.random() * 0xffffff),
+			})
+		);
+
+		// Draw the outline of the note
+		const geometry = new EdgesGeometry(square.geometry);
+		const material = new LineBasicMaterial({ color: 0xffffff });
+		const wireframe = new LineSegments(geometry, material);
+		// Set the note & outline position
+		wireframe.position.set(i, 0, 0);
+		square.position.set(i, 0, 0);
+		// Add the generated note components to the row
+		group.add(square, wireframe);
+	}
+
+	const groupSize = new Box3().setFromObject(group).getSize(new Vector3());
+	const xAxisOffset = -groupSize.x / 2 + 0.5;
+
+	group.position.set(xAxisOffset, 0, 0);
+	scene.add(group);
+
+	// Dev-only
+	// Show the coordinate system axes
+	scene.add(new AxesHelper(5));
+
+	/**
+	 * Execute once the webpage has been mounted.
+	 */
+	onMounted(() => {
+		renderer = new WebGLRenderer({
+			canvas: canvas.value ?? undefined,
+			alpha: true,
+		});
+		renderer.setPixelRatio(window.devicePixelRatio);
+		renderer.setSize(width.value, height.value);
+		renderer.render(scene, camera);
+	});
+</script>
+
+<template>
+	<canvas ref="canvas" />
+</template>
+
+<style>
+	:root {
+		color-scheme: light dark;
+		color: rgba(255, 255, 255, 0.87);
+		background-color: #242424;
+
+		font-synthesis: none;
+		text-rendering: optimizeLegibility;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		-webkit-text-size-adjust: 100%;
+	}
+
+	html,
+	body {
+		margin: 0;
+	}
+
+	canvas {
+		display: block;
+	}
+</style>
